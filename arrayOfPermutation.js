3d521731e399f64d5b25db5977e4bff1e2ecbbca
onlyhamzashahid <hamza.7285618@gmail.com>
2024-05-21 15:59:48 +0500

    1920 Buld Array Of Permutation

---
+++ arrayOfPermutation.js
@@ -0,0 +1,17 @@
+
+
+
+function buildArray (nums) {
+const n = nums.length;
+const ans = new Array(n);
+
+for (let i = 0; i < n; i++) {
+  ans[i] = Math.floor((n * nums[nums[i] % n] + nums[i]) / n);
+}
+
+return ans;
+
+};
+
+const result = buildArray([0, 2, 1, 5, 3, 4]);
+console.log(result)
---
+++ twosum.js
@@ -0,0 +1,18 @@
+var twoSum = function(nums, target) {
+    
+  let map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    let num1 = nums[i];
+    let num2 = target - num1
+    if (map.has(num2)) {
+        return [i , map.get(num2)]
+    }
+    map.set(num1, i)
+    
+  }
+
+};
+
+
+  const result = twoSum([2, 7, 11, 15], target= 9);
+  console.log(result);
