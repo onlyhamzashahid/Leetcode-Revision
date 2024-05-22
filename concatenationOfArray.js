a5aa7987f3e01dcc090e6d2384c662852e340c8e
onlyhamzashahid <hamza.7285618@gmail.com>
2024-05-22 10:49:32 +0500

    1929 Concatenation of Array

---
+++ concatenationOfArray.js
@@ -0,0 +1,9 @@
+function concatArray(nums) {
+    let one = nums;
+    let two= nums;
+    let result = one.concat(two);
+    return result;
+}
+
+let check = concatArray([1,2,3])
+console.log(check)
